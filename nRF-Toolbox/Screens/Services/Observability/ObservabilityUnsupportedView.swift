0ea080b6d5cea184a7e7b08f1cb29ecc99ee391b
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2026-07-06 15:08:05 +0200

    Implement MPS not available section.

--- nRF-Toolbox/Screens/Services/Observability/ObservabilityUnsupportedView.swift
+++ nRF-Toolbox/Screens/Services/Observability/ObservabilityUnsupportedView.swift
@@ -14,6 +14,17 @@ struct ObservabilityUnsupportedView: View {
     // MARK: view
 
     var body: some View {
-        Text("Memfault unsupported")
+        VStack(spacing: 16) {
+            Image("device_manager")
+                .renderingMode(.template)
+                .resizable()
+                .foregroundColor(.nordicBlue)
+                .frame(width: 56, height: 56)
+            
+            Text("MDS is not supported")
+            
+            Text("MDS service is not available in the current version of the app. Please use the nRF Connect Device Manager app from Nordic Semiconductor to update your device's firmware.")
+                .font(.caption)
+        }
     }
 }
