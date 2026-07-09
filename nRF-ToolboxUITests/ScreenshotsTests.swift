3f1ece8c486c7acea30ab266b9e04bea2ddb1930
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2026-07-09 09:33:26 +0200

    Fix screenshots test.

--- nRF-ToolboxUITests/ScreenshotsTests.swift
+++ nRF-ToolboxUITests/ScreenshotsTests.swift
@@ -35,41 +35,39 @@ final class ScreenshotsTests: XCTestCase {
         snapshot("ScannerScreen")
         
         sleep(sleepTime)
-        app/*@START_MENU_TOKEN@*/.staticTexts["Cycling Speed and Cadence sensor"]/*[[".buttons[\"Cycling Speed and Cadence sensor\"].staticTexts",".buttons.staticTexts[\"Cycling Speed and Cadence sensor\"]",".staticTexts[\"Cycling Speed and Cadence sensor\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
-        sleep(sleepTime)
-        app/*@START_MENU_TOKEN@*/.buttons["device_item_0"]/*[[".buttons",".containing(.other, identifier: nil)",".containing(.staticText, identifier: \"Cycling Sensor\")",".containing(.image, identifier: \"cpu\")",".otherElements",".buttons[\"Cycling Sensor\"]",".buttons[\"device_item_0\"]"],[[[-1,6],[-1,5],[-1,4,2],[-1,0,1]],[[-1,3],[-1,2],[-1,1]],[[-1,6],[-1,5]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
+        app.staticTexts["Cycling Speed and Cadence sensor"].firstMatch.tap()
         
         sleep(sleepTime)
         snapshot("CSCSScreen")
-        
-        let backButton = app/*@START_MENU_TOKEN@*/.buttons["nRF Toolbox"]/*[[".navigationBars",".buttons",".buttons[\"nRF Toolbox\"]"],[[[-1,2],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.firstMatch
         sleep(sleepTime)
-        if backButton.exists { backButton.tap() }
+        if app.buttons["nRF Toolbox"].firstMatch.exists {
+            app.buttons["nRF Toolbox"].firstMatch.tap()
+        }
+        
         sleep(sleepTime)
         scannerButton.tap()
         
         sleep(sleepTime)
-        app.buttons["Heart rate"].firstMatch.tap()
-        sleep(sleepTime)
-        app/*@START_MENU_TOKEN@*/.buttons["device_item_1"]/*[[".buttons",".containing(.staticText, identifier: \"Heart Rate\")",".containing(.image, identifier: \"heart.fill\")",".containing(.staticText, identifier: \"Heart Rate Sensor\")",".otherElements",".buttons[\"Heart Rate Sensor\"]",".buttons[\"device_item_1\"]"],[[[-1,6],[-1,5],[-1,4,2],[-1,0,1]],[[-1,3],[-1,2],[-1,1]],[[-1,6],[-1,5]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
+        app.staticTexts["Heart rate"].firstMatch.tap()
         
         sleep(sleepTime)
         snapshot("HRSScreen")
-        
-        if backButton.exists { backButton.tap() }
-        app/*@START_MENU_TOKEN@*/.buttons["scannerButton"]/*[[".buttons",".containing(.staticText, identifier: \"Connect to Device\")",".containing(.image, identifier: \"dot.radiowaves.right\")",".otherElements",".buttons[\"Connect to Device\"]",".buttons[\"scannerButton\"]"],[[[-1,5],[-1,4],[-1,3,2],[-1,0,1]],[[-1,2],[-1,1]],[[-1,5],[-1,4]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
+        sleep(sleepTime)
+        if app.buttons["nRF Toolbox"].firstMatch.exists {
+            app.buttons["nRF Toolbox"].firstMatch.tap()
+        }
+
+        app.buttons["scannerButton"].firstMatch.tap()
         
         sleep(sleepTime)
-        app.buttons["Blood pressure"].firstMatch.tap()
-        sleep(sleepTime)
-        app/*@START_MENU_TOKEN@*/.buttons["device_item_2"]/*[[".buttons",".containing(.staticText, identifier: \"Nordic UART Service\")",".containing(.image, identifier: \"character.cursor.ibeam\")",".containing(.staticText, identifier: \"UART\")",".otherElements",".buttons[\"UART\"]",".buttons[\"device_item_2\"]"],[[[-1,6],[-1,5],[-1,4,2],[-1,0,1]],[[-1,3],[-1,2],[-1,1]],[[-1,6],[-1,5]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
+        app.staticTexts["Blood pressure"].firstMatch.tap()
         
         sleep(sleepTime)
         snapshot("BPSScreen")
         sleep(sleepTime)
-        if backButton.exists {
-            backButton.tap()
-            snapshot("MainScreen")
+        if app.buttons["nRF Toolbox"].firstMatch.exists {
+            app.buttons["nRF Toolbox"].firstMatch.tap()
+            snapshot("MainScreen") // Only for phones (not tablets)
         }
     }
 }
