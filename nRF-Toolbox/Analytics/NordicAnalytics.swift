2f870065165ff28d7b3b7a77317ab40a03247357
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2026-08-06 11:15:34 +0200

    Send analytics events when clicking links.

--- nRF-Toolbox/Analytics/NordicAnalytics.swift
+++ nRF-Toolbox/Analytics/NordicAnalytics.swift
@@ -11,15 +11,19 @@ import FirebaseAnalytics
 
 class NordicAnalytics {
     
+    private static let isEnabledKey = "analytics.enabled"
+    private static let storage = UserDefaults(suiteName: "analytics")!
+    
     static func configure() {
         FirebaseApp.configure()
     }
     
     static func logEvent(_ event: AnalyticsEvent) {
+        guard storage.bool(forKey: isEnabledKey) else { return }
         Analytics.logEvent(event.name, parameters: event.parameters)
     }
     
     static func setAnalyticsEnabled(_ enabled: Bool) {
-        
+        storage.set(enabled, forKey: isEnabledKey)
     }
 }
