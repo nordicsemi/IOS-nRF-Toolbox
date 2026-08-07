4d705ed75da4093d076737d6aa9866f2c01c7bff
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2026-08-07 14:58:24 +0200

    Remove unused items.

--- nRF-Toolbox/Analytics/AnalyticsString.swift
+++ nRF-Toolbox/Analytics/AnalyticsString.swift
@@ -40,24 +40,4 @@ enum AnalyticsString : LocalizedStringKey {
     case stateUndecidedDetail = "You haven't made a choice yet. Statistics are off until you turn them on."
 
     case done = "Done"
-
-    case description = """
-You can help improve our application by sharing anonymous statistics on how you interact with the app. You can enable or disable this feature at any time in Settings.
-
-**What kind of information do we collect?**
-
-We collect only anonymous information about user interaction with the application. For example, we may measure how often a feature is used by counting the number of times the button that opens it is tapped.
-
-Data provided by the user is never collected.
-
-**How do we use this information?**
-
-The information is used to analyze user interaction with the application and identify areas for improvement.
-
-**How is this information processed and shared?**
-
-Firebase is used to collect and process the data.
-
-We do not share the data with any third-party companies or individuals other than Google (Firebase).
-"""
 }
